0d4a507db970983f152d2b0b1e4c1c1d0d450012
Shizene Maredia <s_m971@txstate.edu>
2022-04-11 22:35:53 -0500

    initialize repo

---
+++ app/src/main/java/hfpocketwhiz/com/LoginActivity.kt
@@ -0,0 +1,17 @@
+package hfpocketwhiz.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView as AndroidWidgetTextView
+import android.widget.*
+import android.widget.TextView as TextView1
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        //AndroidWidgetTextView btn = findViewById(R.id.register)
+
+
+    }
+}
