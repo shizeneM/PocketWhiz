a36035d4321b6ac269c143274f2dbd69b5f16827
Shizene Maredia <s_m971@txstate.edu>
2022-04-14 17:00:04 -0500

    Adding changes

--- app/src/main/java/hfpocketwhiz/com/LoginActivity.kt
+++ app/src/main/java/hfpocketwhiz/com/LoginActivity.kt
@@ -3,50 +3,23 @@ package hfpocketwhiz.com
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
-import android.view.View
-import android.widget.TextView as AndroidWidgetTextView
 import android.widget.*
 import hfpocketwhiz.com.databinding.ActivityLoginBinding
-import android.widget.TextView as TextView1
 
-class LoginActivity() : AppCompatActivity(), Parcelable {
-    private lateinit var binding:ActivityLoginBinding
-
-    constructor(parcel: Parcel) : this() {
-
-    }
+class LoginActivity() : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         supportActionBar?.hide()
 
-        val viewBinding= ActivityLoginBinding.inflate(layoutInflater)
+        val viewBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
         viewBinding.loginbutton.setOnClickListener {
             val loginintent = Intent(this@LoginActivity, RegisterActivity::class.java)
             startActivity(loginintent)
-        }
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<LoginActivity> {
-        override fun createFromParcel(parcel: Parcel): LoginActivity {
-            return LoginActivity(parcel)
-        }
-
-        override fun newArray(size: Int): Array<LoginActivity?> {
-            return arrayOfNulls(size)
         }
     }
 }
