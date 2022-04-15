fa71915550941a906d7930e1a62b98a3791f04da
Shizene Maredia <s_m971@txstate.edu>
2022-04-14 22:44:53 -0500

    new changes

--- app/src/main/java/hfpocketwhiz/com/MainActivity.kt
+++ app/src/main/java/hfpocketwhiz/com/MainActivity.kt
@@ -1,17 +1,14 @@
 package hfpocketwhiz.com
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val intent = getIntent()
+        val intent = intent
         
     }
 }
