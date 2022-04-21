9066da4c850306d914c0ba41e1e88a4a6ef46a97
Irene Marmolejo <irenemarmolejo@Irenes-MBP.austin.rr.com>
2022-04-21 11:10:12 -0500

    lancher logo change

--- app/src/main/java/hfpocketwhiz/com/SplashScreen.kt
+++ app/src/main/java/hfpocketwhiz/com/SplashScreen.kt
@@ -1,11 +1,13 @@
 package hfpocketwhiz.com
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
