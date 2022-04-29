6c11203bccd2d4ec0603a4df62b7186a2857c526
Shizene Maredia <s_m971@txstate.edu>
2022-04-28 23:38:55 -0500

    commiting changes

--- app/src/main/java/hfpocketwhiz/com/MainAppLayout.kt
+++ app/src/main/java/hfpocketwhiz/com/MainAppLayout.kt
@@ -1,12 +1,22 @@
 package hfpocketwhiz.com
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import hfpocketwhiz.com.databinding.ActivityMainAppLayoutBinding
 
 class MainAppLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_app_layout)
         supportActionBar?.hide()
+
+
+        val viewBinding = ActivityMainAppLayoutBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.airplane.setOnClickListener {
+            startActivity(Intent(this@MainAppLayout, LoginActivity::class.java))
+        }
     }
 }
